515947b05d14667cc9009d1980e0d148eda2de5b
jasonandjay <342690199@qq.com>
2019-09-18 14:36:12 +0800

    固定侧边栏，同时不影响footer

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -5,7 +5,7 @@ export default {
   contentWidth: 'Fluid',
   fixedHeader: false,
   autoHideHeader: false,
-  fixSiderbar: false,
+  fixSiderbar: true,
   colorWeak: false,
   menu: {
     locale: true,
