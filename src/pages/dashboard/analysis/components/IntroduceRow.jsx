23af5541b602dbfb578c9a3d4d038849f4f511c6
dreamSeekerYu <3024637102@qq.com>
2019-09-18 14:15:20 +0800

    '修改概览下面的分析页'

--- src/pages/dashboard/analysis/components/IntroduceRow.jsx
+++ src/pages/dashboard/analysis/components/IntroduceRow.jsx
@@ -2,7 +2,7 @@ import { Col, Icon, Row, Tooltip } from 'antd';
 import { FormattedMessage } from 'umi-plugin-react/locale';
 import React from 'react';
 import numeral from 'numeral';
-import { ChartCard, MiniArea, MiniBar, MiniProgress, Field } from './Charts';
+import { ChartCard, MiniArea, MiniBar, TimelineChart, MiniProgress, Field } from './Charts';
 import Trend from './Trend';
 import Yuan from '../utils/Yuan';
 import styles from '../style.less';
@@ -18,7 +18,7 @@ const topColResponsiveProps = {
   },
 };
 
-const IntroduceRow = ({ loading, visitData }) => (
+const IntroduceRow = ({ loading, visitData, visitData2 }) => (
   <Row gutter={24} type="flex">
     <Col {...topColResponsiveProps}>
       <ChartCard
@@ -56,7 +56,8 @@ const IntroduceRow = ({ loading, visitData }) => (
         }
         contentHeight={46}
       >
-        <Trend
+        <MiniArea line height={45} data={visitData2} />
+        {/* <Trend
           flag="up"
           style={{
             marginRight: 16,
@@ -64,14 +65,88 @@ const IntroduceRow = ({ loading, visitData }) => (
         >
           <FormattedMessage id="dashboard-analysis.analysis.week" defaultMessage="Weekly Changes" />
           <span className={styles.trendText}>12%</span>
-        </Trend>
-        <Trend flag="down">
+        </Trend> */}
+        {/* <Trend flag="down">
           <FormattedMessage id="dashboard-analysis.analysis.day" defaultMessage="Daily Changes" />
           <span className={styles.trendText}>11%</span>
-        </Trend>
+        </Trend> */}
       </ChartCard>
     </Col>
-
+    <Col {...topColResponsiveProps}>
+      <ChartCard
+        loading={loading}
+        bordered={false}
+        title={
+          <FormattedMessage
+            id="dashboard-analysis.analysis.operational-effect"
+            defaultMessage="Operational Effect"
+          />
+        }
+        action={
+          <Tooltip
+            title={
+              <FormattedMessage
+                id="dashboard-analysis.analysis.introduce"
+                defaultMessage="Introduce"
+              />
+            }
+          >
+            <Icon type="info-circle-o" />
+          </Tooltip>
+        }
+        total="780000"
+        footer={
+          <Field
+            label={
+              <FormattedMessage
+                id="dashboard-analysis.analysis.conversion-rate"
+                defaultMessage="Conversion Rate"
+              />
+            }
+            value="234"
+          />
+        }
+        contentHeight={46}
+      >
+        <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
+      </ChartCard>
+    </Col>
+    <Col {...topColResponsiveProps}>
+      <ChartCard
+        bordered={false}
+        loading={loading}
+        title={
+          <FormattedMessage id="dashboard-analysis.analysis.payments" defaultMessage="Payments" />
+        }
+        action={
+          <Tooltip
+            title={
+              <FormattedMessage
+                id="dashboard-analysis.analysis.introduce"
+                defaultMessage="Introduce"
+              />
+            }
+          >
+            <Icon type="info-circle-o" />
+          </Tooltip>
+        }
+        total={numeral(6560).format('0,0')}
+        footer={
+          <Field
+            label={
+              <FormattedMessage
+                id="dashboard-analysis.analysis.conversion-rate"
+                defaultMessage="Conversion Rate"
+              />
+            }
+            value="5555"
+          />
+        }
+        contentHeight={46}
+      >
+        <MiniBar data={visitData} />
+      </ChartCard>
+    </Col>
     <Col {...topColResponsiveProps}>
       <ChartCard
         bordered={false}
@@ -104,98 +179,6 @@ const IntroduceRow = ({ loading, visitData }) => (
         contentHeight={46}
       >
         <MiniArea color="#975FE4" data={visitData} />
-      </ChartCard>
-    </Col>
-    <Col {...topColResponsiveProps}>
-      <ChartCard
-        bordered={false}
-        loading={loading}
-        title={
-          <FormattedMessage id="dashboard-analysis.analysis.payments" defaultMessage="Payments" />
-        }
-        action={
-          <Tooltip
-            title={
-              <FormattedMessage
-                id="dashboard-analysis.analysis.introduce"
-                defaultMessage="Introduce"
-              />
-            }
-          >
-            <Icon type="info-circle-o" />
-          </Tooltip>
-        }
-        total={numeral(6560).format('0,0')}
-        footer={
-          <Field
-            label={
-              <FormattedMessage
-                id="dashboard-analysis.analysis.conversion-rate"
-                defaultMessage="Conversion Rate"
-              />
-            }
-            value="60%"
-          />
-        }
-        contentHeight={46}
-      >
-        <MiniBar data={visitData} />
-      </ChartCard>
-    </Col>
-    <Col {...topColResponsiveProps}>
-      <ChartCard
-        loading={loading}
-        bordered={false}
-        title={
-          <FormattedMessage
-            id="dashboard-analysis.analysis.operational-effect"
-            defaultMessage="Operational Effect"
-          />
-        }
-        action={
-          <Tooltip
-            title={
-              <FormattedMessage
-                id="dashboard-analysis.analysis.introduce"
-                defaultMessage="Introduce"
-              />
-            }
-          >
-            <Icon type="info-circle-o" />
-          </Tooltip>
-        }
-        total="78%"
-        footer={
-          <div
-            style={{
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-            }}
-          >
-            <Trend
-              flag="up"
-              style={{
-                marginRight: 16,
-              }}
-            >
-              <FormattedMessage
-                id="dashboard-analysis.analysis.week"
-                defaultMessage="Weekly Changes"
-              />
-              <span className={styles.trendText}>12%</span>
-            </Trend>
-            <Trend flag="down">
-              <FormattedMessage
-                id="dashboard-analysis.analysis.day"
-                defaultMessage="Weekly Changes"
-              />
-              <span className={styles.trendText}>11%</span>
-            </Trend>
-          </div>
-        }
-        contentHeight={46}
-      >
-        <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
       </ChartCard>
     </Col>
   </Row>
